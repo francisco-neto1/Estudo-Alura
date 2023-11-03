6315408b8c848d8be94aa3d970237cbd9ae3242c
Francisco Neto <franciscon3t02018@hotmail.com>
2023-11-03 10:46:25 -0300

    C#

---
+++ NumeroAleatorio/Program.cs
@@ -0,0 +1,25 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("\nParabéns! Você acertou o número.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("\nO número é maior.");
+    }
+    else
+    {
+        Console.WriteLine("\nO número é menor.");
+    }
+
+} while (true);
+
+Console.WriteLine("\nO jogo acabou. Você acertou o número secreto!");
